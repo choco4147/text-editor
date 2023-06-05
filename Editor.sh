25d1805e53679a1f65f36bf4ec78e91a0b83fb79
choco4147 <100542480+choco4147@users.noreply.github.com>
2023-06-04 19:35:41 -0500

    a lot of changes
    too lazy to name them all

--- Editor.sh
+++ Editor.sh
@@ -94,7 +94,75 @@ function  deleteFile {
 }
 function edit {
     echo "editing file \""$FILE"\"."
-    
+    CONTENT=$(cat $FILE)
+    echo $CONTENT
+    EDITING=1
+    CURSORLOC=${#CONTENT}
+
+    while [ $EDITING != 0 ]; 
+    do 
+        CONTENT="${CONTENT:0:$CURSORLOC}""${CONTENT:$CURSORLOC+1}"
+        LENGTH=${#CONTENT}
+        read -s -n 1 key
+        read -s -n 4 arw
+        
+        if [ "$key" = "0" ]; 
+        then 
+            EDITING=0 
+        fi
+
+        if [ "$key" = "-" ]; 
+        then
+            if [ $CURSORLOC -gt 0 ]; 
+            then
+                let "CURSORLOC=CURSORLOC-1"
+            fi
+        fi 
+
+        if [ "$key" = "=" ];
+        then
+            if [ $CURSORLOC -lt $LENGTH ];
+            then
+                let "CURSORLOC=CURSORLOC+1"
+            fi
+        fi
+
+        if [ "$key" = "9" ];
+        then
+            if [ $CURSORLOC -gt 0 ];
+            then
+                CONTENT="${CONTENT:0:$CURSORLOC-1}""${CONTENT:$CURSORLOC}"
+                let "CURSORLOC=CURSORLOC-1"
+            fi
+        fi
+
+        if [ "$key" = "8" ];
+        then
+            CONTENT="${CONTENT:0:$CURSORLOC} ${CONTENT:$CURSORLOC}"
+        fi
+
+        if [ "$key" != "0" ] && [ "$key" != "-" ] && [ "$key" != "=" ] && [ "$key" != "9" ] && [ "$key" != "8" ];
+        then 
+            CONTENT="${CONTENT:0:$CURSORLOC}""$key""${CONTENT:$CURSORLOC}"
+            let "CURSORLOC=CURSORLOC+1"
+        fi
+        clear
+        CONTENT="${CONTENT:0:$CURSORLOC}""▮""${CONTENT:$CURSORLOC}"
+        echo $CONTENT
+
+    done
+    printf "\n\n\n\n"
+    printf "save modified file? (y/n) "
+    read YN
+    if [ "$YN" = "n" ];
+    then 
+        echo "not saving file, exiting program."
+        exit 6
+    fi
+    rm $FILE
+    touch $FILE
+    CONTENT="${CONTENT:0:$CURSORLOC}""${CONTENT:$CURSORLOC+1}"
+    echo $CONTENT >> $FILE
 }
 
 echo "Would you like to edit (e), create (c), or delete (d) a file?"
